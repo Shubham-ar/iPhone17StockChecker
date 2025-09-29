ffda031f11e272bce6592e8a4abe17aed67b1294
Shubham Jhamb <shubham.jhamb2000@gmail.com>
2025-09-28 22:32:58 -0400

    resolved gh actions error

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ const APPLE_API_URL = `https://www.apple.com/ca/shop/fulfillment-messages?fae=tr
 
 async function fetchStock() {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
